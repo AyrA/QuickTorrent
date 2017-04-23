9a6f5e4bbb6332c934717bcf9b7c679572cf5e8c
Kevin Gut <kevin.gut@bitm.sg>
2017-04-24 00:14:45 +0200

    First working version
    - This commit excludes the self made monotorrent library

---
+++ QuickTorrent/Program.cs
@@ -0,0 +1,134 @@
+﻿using MonoTorrent;
+using MonoTorrent.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace QuickTorrent
+{
+    class Program
+    {
+        private static List<TorrentHandler> Handler;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (!args.Contains("/?"))
+                {
+                    foreach (var arg in args)
+                    {
+                        if (arg.ToLower().StartsWith("magnet:"))
+                        {
+                            Handler.Add(new TorrentHandler(ParseLink(arg)));
+                        }
+                        else if (File.Exists(arg))
+                        {
+                            Handler.Add(new TorrentHandler(ParseTorrent(arg)));
+                        }
+                        else if (ValidHex(arg))
+                        {
+                            Handler.Add(new TorrentHandler(ParseHash(arg)));
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine("Invalid Argument. Only file names, magnet links and hashes are supported.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine(@"QuickTorrent.exe [TorrentFile [...]] [MagnetLink [...]] [InfoHash [...]]
+
+TorrentFile  - Torrent file to download
+MagnetLink   - Magnet link to download that can be found in the DHT network
+InfoHash     - SHA1 hash of a torrent that can be found in the DHT network
+
+Without arguments the application tries to interpret its file name as a hash.");
+                }
+            }
+            else
+            {
+            }
+        }
+
+        private static MagnetLink ParseLink(string Arg)
+        {
+            return new MagnetLink(Arg);
+        }
+
+        private static InfoHash ParseHash(string Arg)
+        {
+            return new InfoHash(s2b(Arg));
+        }
+
+        private static Torrent ParseTorrent(string Arg)
+        {
+            return Torrent.Load(File.ReadAllBytes(Arg));
+        }
+
+        private static bool ValidHex(string hexString)
+        {
+            try
+            {
+                s2b(hexString);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static byte[] s2b(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(string.Format(CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+        }
+
+        private static void TH_PiecemapUpdate(object Sender, PiecemapEventArgs Args)
+        {
+            const char EMPTY = '░';
+            const char FULL = '█';
+            //const string TOP = "╔═╗";
+            //const char LR = '║';
+            //const string BOTTOM = "╚═╝";
+            var Handler = (TorrentHandler)Sender;
+
+            if (Args.Piecemap != null)
+            {
+                double count = Console.BufferWidth * (Console.WindowHeight - 1);
+                var SB = new StringBuilder((int)count);
+                for (double d = 0; d < count; d++)
+                {
+                    SB.Append(Args.Piecemap[(int)(Args.Piecemap.Length * d / count)] ? FULL : EMPTY);
+                }
+                lock ("PIECE_PRINT")
+                {
+                    var ProgressText = $"QuickTorrent {Handler.TorrentName}; {Math.Round(Handler.Progress, 1)}%; State: {Handler.State}; S/L: {Handler.Seeds}/{Handler.Leechs}";
+                    if (Console.Title != ProgressText)
+                    {
+                        Console.Title = ProgressText;
+                    }
+                    var CurrentMap = SB.ToString();
+                    Console.SetCursorPosition(0, 0);
+                    Console.Write(SB.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ QuickTorrent/TorrentHandler.cs
@@ -0,0 +1,247 @@
+﻿using MonoTorrent;
+using MonoTorrent.Client;
+using MonoTorrent.Client.Encryption;
+using MonoTorrent.Common;
+using MonoTorrent.Dht;
+using MonoTorrent.Dht.Listeners;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace QuickTorrent
+{
+    public struct PiecemapEventArgs
+    {
+        /// <summary>
+        /// The entire piece map. Enabled pieces are already downloaded
+        /// </summary>
+        public readonly bool[] Piecemap;
+        /// <summary>
+        /// The last piece index updated
+        /// </summary>
+        public readonly int LastPiece;
+        public readonly bool Complete;
+
+        public PiecemapEventArgs(bool[] Piecemap, int LastPiece)
+        {
+            this.Piecemap = Piecemap;
+            this.LastPiece = LastPiece;
+            Complete = Piecemap != null && Piecemap.All(m => m);
+        }
+    }
+
+    public class TorrentHandler : IDisposable
+    {
+        public delegate void PiecemapUpdateHandler(object Sender, PiecemapEventArgs Args);
+
+        public event PiecemapUpdateHandler PiecemapUpdate = delegate { };
+
+        public const string DOWNLOAD_DIR = @"%USERPROFILE%\Downloads\QuickTorrent";
+        public const string TORRENT_DIR = @"%APPDATA%\QuickTorrent\TorrentCache";
+        public const string DHTFILE = @"%APPDATA%\QuickTorrent\dht.bin";
+
+        private static DhtListener DL;
+        private static DhtEngine DE;
+        private static ClientEngine CE;
+        private static EngineSettings ES;
+
+        private TorrentManager TM;
+        private TorrentSettings TS;
+
+        public bool IsComplete
+        {
+            get
+            {
+                return TM.State == TorrentState.Seeding;
+            }
+        }
+        public bool IsPaused
+        {
+            get
+            {
+                return TM.State == TorrentState.Paused;
+            }
+        }
+        public bool IsDownloading
+        {
+            get
+            {
+                return TM.State == TorrentState.Downloading ||
+                    TM.State == TorrentState.Hashing ||
+                    TM.State == TorrentState.Metadata;
+            }
+        }
+        public double Progress
+        {
+            get
+            {
+                return TM.Progress;
+            }
+        }
+        public TorrentState State
+        {
+            get
+            {
+                return TM.State;
+            }
+        }
+        public int ConnectedPeers
+        {
+            get
+            {
+                return TM.OpenConnections;
+            }
+        }
+        public int Seeds
+        {
+            get
+            {
+                return TM.Peers.Seeds;
+            }
+        }
+        public int Leechs
+        {
+            get
+            {
+                return TM.Peers.Leechs;
+            }
+        }
+        public string TorrentName
+        {
+            get
+            {
+                return TM.Torrent.Name;
+            }
+        }
+
+        private bool[] PieceMap;
+
+        public bool[] Map
+        {
+            get
+            {
+                return PieceMap == null ? null : (bool[])PieceMap.Clone();
+            }
+        }
+
+        public TorrentHandler(MagnetLink ML, string DownloadDir = DOWNLOAD_DIR)
+        {
+            InitBase();
+            TM = new TorrentManager(ML, Environment.ExpandEnvironmentVariables(DownloadDir), TS, Environment.ExpandEnvironmentVariables(TORRENT_DIR));
+            Assign();
+
+        }
+
+        public TorrentHandler(Torrent T, string DownloadDir = DOWNLOAD_DIR)
+        {
+            InitBase();
+            PieceMap = new bool[T.Pieces.Count];
+            TM = new TorrentManager(T, Environment.ExpandEnvironmentVariables(DownloadDir), TS);
+            Assign();
+
+        }
+
+        public TorrentHandler(InfoHash H, string DownloadDir = DOWNLOAD_DIR)
+        {
+            InitBase();
+            TM = new TorrentManager(H, DownloadDir, TS, TORRENT_DIR, new List<RawTrackerTier>());
+            Assign();
+        }
+
+        public void Start()
+        {
+            TM.Start();
+        }
+
+        public void Stop()
+        {
+            TM.Stop();
+        }
+
+        private void InitBase()
+        {
+            if (!Directory.Exists(Environment.ExpandEnvironmentVariables(DOWNLOAD_DIR)))
+            {
+                Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(DOWNLOAD_DIR));
+            }
+            if (!Directory.Exists(Environment.ExpandEnvironmentVariables(TORRENT_DIR)))
+            {
+                Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(TORRENT_DIR));
+            }
+
+            //Initialize DHT engine for the first time
+            lock ("DHT_Create")
+            {
+                if (DE == null)
+                {
+                    DL = new DhtListener(new IPEndPoint(IPAddress.Any, 54321));
+                    DE = new DhtEngine(DL);
+                    if (File.Exists(Environment.ExpandEnvironmentVariables(DHTFILE)))
+                    {
+                        DE.Start(File.ReadAllBytes(Environment.ExpandEnvironmentVariables(DHTFILE)));
+                    }
+                    else
+                    {
+                        DE.Start();
+                    }
+                    DL.Start();
+                }
+            }
+            lock ("CE_Create")
+            {
+                if (ES == null)
+                {
+                    ES = new EngineSettings(Environment.ExpandEnvironmentVariables(DOWNLOAD_DIR), 54321, 100, 50, 0, 0, EncryptionTypes.All);
+                }
+                if (CE == null)
+                {
+                    CE = new ClientEngine(ES);
+                    CE.RegisterDht(DE);
+                }
+            }
+            TS = new TorrentSettings(10);
+        }
+
+        private void Assign()
+        {
+            TM.PieceHashed += (a, b) =>
+            {
+                //if the PieceMap is null, this was a magnet link torrent but we know the map now
+                if (PieceMap == null)
+                {
+                    PieceMap = new bool[TM.Torrent.Pieces.Count];
+                }
+                lock (PieceMap)
+                {
+                    PieceMap[b.PieceIndex] = b.HashPassed;
+                }
+                PiecemapUpdate(this, new PiecemapEventArgs(Map, b.PieceIndex));
+            };
+            TM.PieceManager.BlockReceived += (a, b) =>
+            {
+                PieceMap[b.Piece.Index] = true;
+                PiecemapUpdate(this, new PiecemapEventArgs(Map, b.Piece.Index));
+            };
+
+            CE.Register(TM);
+        }
+
+        public void Dispose()
+        {
+            if (TM != null && CE != null)
+            {
+                CE.Unregister(TM);
+                Stop();
+                TM.Dispose();
+                TM = null;
+            }
+        }
+
+        public static void SaveDhtNodes()
+        {
+            File.WriteAllBytes(Environment.ExpandEnvironmentVariables(DHTFILE), DE.SaveNodes());
+        }
+    }
+}
